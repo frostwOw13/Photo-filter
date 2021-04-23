ba2b9eebfd4d94c3d383da26a03646471622c4da
frostwOw13 <frostwow13@yandex.ru>
2021-04-23 22:42:53 +0300

    feat: add reset button

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -7,3 +7,24 @@ inputs.addEventListener('input', (e) => {
   filter.nextElementSibling.innerHTML = filter.value; // Set output value
   document.documentElement.style.setProperty(`--${filter.name}`, filter.value + suffix);
 })
+
+// When pressed Reset button
+const resetBtn = document.querySelector('.btn-reset');
+const inputsArr = document.querySelectorAll('.filters input')
+
+resetBtn.addEventListener('click', (e) => {
+  inputsArr.forEach((input) => {
+    const suffix = input.dataset.sizing;
+    if (input.name == 'saturate') {
+      input.value = 100;
+      input.nextElementSibling.innerHTML = input.value;
+      document.documentElement.style.setProperty(`--${input.name}`, input.value + suffix);
+    } else {
+      input.value = 0;
+      input.nextElementSibling.innerHTML = input.value;
+      document.documentElement.style.setProperty(`--${input.name}`, input.value + suffix);
+    }
+  });
+})
+
+// When pressed Next Picture Button
